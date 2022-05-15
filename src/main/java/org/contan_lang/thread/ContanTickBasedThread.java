4b3e88062d25482dbc8df1132345808ee998dbf6
Be4rJP <bea0224@outlook.jp>
2022-05-16 01:46:04 +0900

    ヨシ！

--- src/main/java/org/contan_lang/thread/ContanTickBasedThread.java
+++ src/main/java/org/contan_lang/thread/ContanTickBasedThread.java
@@ -1,7 +1,6 @@
 package org.contan_lang.thread;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 
 public interface ContanTickBasedThread extends ContanThread {
     
@@ -12,9 +11,7 @@ public interface ContanTickBasedThread extends ContanThread {
      *
      * @param delay Time to delay, specified in ticks.
      *              For the tick interval, check the execution environment settings.
-     *
-     * @return {@link Future} of task
      */
-    <T> Future<T> scheduleTask(Callable<T> task, long delay);
+    <T> void scheduleTask(Callable<T> task, long delay);
     
 }
